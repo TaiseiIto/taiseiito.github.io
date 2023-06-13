101bb8fa537d1f34fe65265c0ac58b1ecd2381d7
TaiseiIto <taisei1212@outlook.jp>
2023-06-13 21:31:08 +0900

    4.2

---
+++ docs/practice1/chapter4/script.js
@@ -0,0 +1,31 @@
+﻿var pieData = {
+	labels: ['円グラフ項目1', '円グラフ項目2', '円グラフ項目3'],
+	series: [1, 2, 3],
+};
+
+var pieOptions = {
+	width: '512px',
+	height: '512px',
+};
+
+new Chartist.Pie('.pie-chart', pieData, pieOptions);
+
+var barData = {
+	labels: ['棒グラフ項目1', '棒グラフ項目2', '棒グラフ項目3'],
+	series: [[3, 2, 1]],
+};
+
+var barOptions = {
+	axisY: {
+		offset: 100,
+		scaleMinSpace: 100,
+		labelInterpolationFnc: function(value) {
+			return value + '単位'
+		},
+	},
+	width: '512px',
+	height: '512px',
+};
+
+new Chartist.Bar('.bar-chart', barData, barOptions);
+
