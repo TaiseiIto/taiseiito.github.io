5489e360efcfbeedae5e3a364db8916ef3324538
TaiseiIto <taisei1212@outlook.jp>
2023-06-17 15:52:22 +0900

    5.9

---
+++ docs/practice1/chapter5/script.js
@@ -0,0 +1,8 @@
+﻿const btn = document.querySelector('.btn-menu');
+const nav = document.querySelector('.main-nav');
+
+btn.addEventListener('click', () => {
+	nav.classList.toggle('open-menu');
+	console.log('Hello, World!');
+});
+
