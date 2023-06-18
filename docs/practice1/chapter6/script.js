48722e7095ea54068a5e8fbb5f06ffa7ac7dae5d
TaiseiIto <taisei1212@outlook.jp>
2023-06-18 20:42:25 +0900

    6.9

--- docs/practice1/chapter6/script.js
+++ docs/practice1/chapter6/script.js
@@ -1,2 +1,6 @@
-﻿new LuminousGallery(document.querySelectorAll(".grid-gallery"));
+﻿new LuminousGallery(document.querySelectorAll(".grid-gallery"), {}, {
+	caption: function(trigger) {
+		return trigger.querySelector("img").getAttribute("alt");
+	}
+});
 
