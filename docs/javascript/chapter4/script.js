881c2c25809207c4de43ae65267c87868650b704
TaiseiIto <taisei1212@outlook.jp>
2023-06-25 20:35:08 +0900

    4.3

--- docs/javascript/chapter4/script.js
+++ docs/javascript/chapter4/script.js
@@ -1,12 +1,16 @@
 let busyCursorCounter = 0;
 const busyCursorRotatePeriod = 1000;
 const busyCursorFramePeriod = 10;
+const loading = document.querySelector('#loading');
+const busyCursor = document.querySelector('#busy-cursor');
 function rotateBusyCursor() {
-	busyCursor = document.querySelector('#busy-cursor');
-	console.log(busyCursor);
 	busyCursorAngle = busyCursorCounter * 360 / busyCursorRotatePeriod;
 	busyCursor.style.background = `conic-gradient(from ${busyCursorAngle}deg, white, black)`;
 	busyCursorCounter = (busyCursorCounter + busyCursorFramePeriod) % busyCursorRotatePeriod;
 }
 setInterval(rotateBusyCursor, busyCursorFramePeriod);
+function loaded() {
+	loading.classList.add('loaded');
+}
+setTimeout(loaded, 3000);
 
