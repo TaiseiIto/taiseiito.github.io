68821b7e3b768f14a98d43816a2bafd9a53861a3
TaiseiIto <taisei1212@outlook.jp>
2023-06-25 22:00:47 +0900

    4.5

--- docs/javascript/chapter4/script.js
+++ docs/javascript/chapter4/script.js
@@ -3,6 +3,7 @@ const busyCursorRotatePeriod = 1000;
 const busyCursorFramePeriod = 10;
 const loading = document.querySelector('#loading');
 const busyCursor = document.querySelector('#busy-cursor');
+const themeButton = document.querySelector('#theme-button');
 function rotateBusyCursor() {
 	busyCursorAngle = busyCursorCounter * 360 / busyCursorRotatePeriod;
 	busyCursor.style.background = `conic-gradient(from ${busyCursorAngle}deg, white, black)`;
@@ -13,4 +14,9 @@ function loaded() {
 	loading.classList.add('loaded');
 }
 setTimeout(loaded, 3000);
+function changeTheme() {
+	themeButton.classList.toggle('light-theme');
+	themeButton.classList.toggle('dark-theme');
+}
+themeButton.addEventListener('click', changeTheme);
 
