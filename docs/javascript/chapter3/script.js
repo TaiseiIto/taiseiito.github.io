2288a7f96fe02ecbe52aaf90e7bcf23815bd614e
TaiseiIto <taisei1212@outlook.jp>
2023-06-25 16:21:21 +0900

    3.11

--- docs/javascript/chapter3/script.js
+++ docs/javascript/chapter3/script.js
@@ -4,7 +4,13 @@ function changeColor() {
 	const colorText = document.querySelector('#colorText');
 	const main = document.querySelector('main');
 	const color = colorPicker.value;
-	colorText.textContent = `カラーコード:${color}`;
+	if(color === '#ffffff') {
+		colorText.textContent = `カラーコード:white`;
+	} else if(color === '#000000') {
+		colorText.textContent = `カラーコード:black`;
+	} else {
+		colorText.textContent = `カラーコード:${color}`;
+	}
 	main.style.backgroundColor = color;
 }
 
