f7f1b1d7d1035142a3f542b0a72ecfefd72cc517
TaiseiIto <taisei1212@outlook.jp>
2023-07-01 21:17:53 +0900

    5.3

--- docs/javascript/chapter5/script.js
+++ docs/javascript/chapter5/script.js
@@ -1,48 +1,46 @@
 const main = document.querySelector('main');
 
 function range(end) {
-	return {
-		[Symbol.iterator]: function() {
-			let count = 0;
-			return {
-				next: function() {
-					if(count < end) {
-						return {
-							value: count++,
-							done: false
-						};
-					} else {
-						return {
-							value: count,
-							done: true
-						};
-					}
-				},
-			};
-		},
-		forEach: function(callback) {
-			for(index of this) {
-				callback(index)
-			}
-		},
-	};
+ return {
+  [Symbol.iterator]: function() {
+   let count = 0;
+   return {
+    next: function() {
+     if(count < end) {
+      return {
+       value: count++,
+       done: false
+      };
+     } else {
+      return {
+       value: count,
+       done: true
+      };
+     }
+    },
+   };
+  },
+  forEach: function(callback) {
+   for(index of this) {
+    callback(index)
+   }
+  },
+ };
 }
-
-range(10).forEach(function(index) {
-	console.log(index);
-});
 
 function imageSection(imageNumber) {
-	section = document.createElement('section');
-	title = document.createElement('h2');
-	title.innerHTML = `画像${imageNumber}`;
-	section.appendChild(title);
-	image = document.createElement('img');
-	image.src = '../../images/myhead.jpg';
-	image.alt = 'my head';
-	section.appendChild(image);
-	return section
+ section = document.createElement('section');
+ title = document.createElement('h2');
+ title.innerHTML = `画像${imageNumber}`;
+ section.appendChild(title);
+ image = document.createElement('img');
+ image.src = '../../images/myhead.jpg';
+ image.alt = 'my head';
+ section.appendChild(image);
+ return section
 }
 
-main.appendChild(imageSection(0));
+range(16).forEach(function(index) {
+ main.appendChild(imageSection(index));
+});
 
