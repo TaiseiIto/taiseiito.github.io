2f7e8e90bacd1656c21f4eb65bd23534c5576720
TaiseiIto <taisei1212@outlook.jp>
2023-07-01 16:40:17 +0900

    5.3

--- docs/javascript/chapter5/script.js
+++ docs/javascript/chapter5/script.js
@@ -1,4 +1,16 @@
-const images = document.querySelector('#images');
-const image = '<img src="../../images/myhead.jpg" alt="myhead">';
-images.insertAdjacentHTML('beforeend', image);
+const main = document.querySelector('main');
+
+function imageSection(imageNumber) {
+	section = document.createElement('section');
+	title = document.createElement('h2');
+	title.innerHTML = `画像${imageNumber}`;
+	section.appendChild(title);
+	image = document.createElement('img');
+	image.src = '../../images/myhead.jpg';
+	image.alt = 'my head';
+	section.appendChild(image);
+	return section
+}
+
+main.appendChild(imageSection(0));
 
