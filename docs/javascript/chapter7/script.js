6fe4d4681b8d566fed5642483f0beaf77809c01b
TaiseiIto <taisei1212@outlook.jp>
2023-07-02 21:57:49 +0900

    7.3

--- docs/javascript/chapter7/script.js
+++ docs/javascript/chapter7/script.js
@@ -1,2 +1,21 @@
-console.log('Hello, World!');
+const loading = document.getElementById('loading');
+const curtain = document.getElementById('curtain');
+console.log(curtain);
+window.addEventListener('load', function() {
+ loading.animate({
+  opacity: [1, 0],
+  visibility: 'hidden',
+ }, {
+  duration: 2000,
+  easing: 'ease-in-out',
+  fill: 'forwards',
+ });
+ curtain.animate({
+  translate: ['0 100vh', '0 -100vh'],
+ }, {
+  duration: 2000,
+  easing: 'ease-in-out',
+  fill: 'forwards',
+ });
+});
 
