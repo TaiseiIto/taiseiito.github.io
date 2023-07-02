e0c054b54f8c6ed1247571f879dd3a0808cdbac9
TaiseiIto <taisei1212@outlook.jp>
2023-07-02 18:55:48 +0900

    6.7

--- docs/javascript/chapter6/script.js
+++ docs/javascript/chapter6/script.js
@@ -2,6 +2,11 @@ for(h2 of document.getElementsByTagName('h2')) {
  h2.animate({
   opacity: [0, 1],
   translate: ['0 100%', '0 0'],
- }, 1000);
+ }, {
+  duration: 1000,
+  direction: 'alternate',
+  easing: 'ease-in-out',
+  iterations: Infinity,
+ });
 }
 
