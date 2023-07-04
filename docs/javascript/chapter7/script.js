7d79b025a2373d4ab460c04b6cb9491019715363
TaiseiIto <taisei1212@outlook.jp>
2023-07-04 22:18:48 +0900

    7.10

--- docs/javascript/chapter7/script.js
+++ docs/javascript/chapter7/script.js
@@ -43,3 +43,16 @@ for(galleryImage of galleryImages) {
  });
 }
 
+const menu = document.getElementsByTagName('nav')[0];
+const menuButton = document.getElementById('menu-button');
+menuButton.addEventListener('click', function() {
+ console.log(menu);
+ menu.animate({
+  transform: ['translate(100%)', 'translate(0)'],
+ }, {
+  duration: 1000,
+  easing: 'ease-in-out',
+  fill: 'forwards',
+ });
+});
+
