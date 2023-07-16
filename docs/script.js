cd2c6a34d1cae72b67f40af826b0501f79fa27ea
TaiseiIto <taisei1212@outlook.jp>
2023-07-16 21:43:06 +0900

    home page

--- docs/script.js
+++ docs/script.js
@@ -61,22 +61,26 @@ function menuFocus() {
  }
 }
 menu.addEventListener('mouseenter', function() {
- menu.riding = true;
- menuFocus();
+ if(!menu.riding) {
+  menu.riding = true;
+  menuFocus();
+ }
 });
-menu.addEventListener('mouseleave', function(event) {
+header.addEventListener('mouseleave', function(event) {
  const navigationRect = navigation.getBoundingClientRect();
  const mouseX = event.x;
  const mouseY = event.y;
  const mouseInNavigation = navigationRect.left <= mouseX && mouseX <= navigationRect.right && navigationRect.top <= mouseY && mouseY <= navigationRect.bottom;
- if(!mouseInNavigation) {
+ if(menu.riding && !mouseInNavigation) {
   menu.riding = false;
   menuFocus();
  }
 });
 navigation.addEventListener('mouseleave', function() {
- menu.riding = false;
- menuFocus();
+ if(menu.riding) {
+  menu.riding = false;
+  menuFocus();
+ }
 });
 
 // Switch between light mode and dark mode
